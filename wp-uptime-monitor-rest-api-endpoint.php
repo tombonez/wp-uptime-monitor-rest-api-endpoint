5a98d190debdde615191e3949906f19dfe70033b
Tom Taylor <tom@twisted.digital>
2023-08-08 00:52:50 +0100

    Removed WPDB Query

--- wp-uptime-monitor-rest-api-endpoint.php
+++ wp-uptime-monitor-rest-api-endpoint.php
@@ -4,7 +4,7 @@
  * Plugin Name:  WP Uptime Monitor REST API Endpoint
  * Plugin URI:   https://github.com/tombonez/wp-uptime-monitor-rest-api-endpoint
  * Description:  A WordPress plugin for adding a non-cacheable REST API endpoint to your website that can be used to monitor its uptime.
- * Version:      1.0.1
+ * Version:      1.1.0
  * Author:       Tom Taylor
  * Author URI:   https://github.com/tombonez
  */
@@ -26,27 +26,13 @@ function register_route() {
 function handle_request() {
 	define( 'DONOTCACHEPAGE', true );
 
-	global $wpdb;
-
-	$query_result = $wpdb->get_row( 'SELECT NOW() as datetime' );
-
-	if ( $query_result ) {
-		return new \WP_REST_Response(
-			array(
-				'status'    => 'online',
-				'timestamp' => $query_result->datetime,
-			),
-			200,
-			wp_get_nocache_headers()
-		);
-	}
-
-	return new \WP_Error(
-		'service_unavailable',
-		'Service Unavailable',
+	return new \WP_REST_Response(
 		array(
-			'status' => 503,
-		)
+			'status'    => 'online',
+			'timestamp' => gmdate( 'Y-m-d H:i:s' ),
+		),
+		200,
+		wp_get_nocache_headers()
 	);
 }
 
